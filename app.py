a085e1176904d675d7f7f31f3fd0974cf1499eb7
Adam Byrne <adamrbyrne@gmail.com>
2023-05-19 11:33:16 +0100

    added restuarant queue functionality

--- app.py
+++ app.py
@@ -2,10 +2,49 @@ from chalice import Chalice
 
 app = Chalice(app_name='lambdalicious')
 
+customerQueue = []
+
 @app.route('/')
 def index():
-    return {"lambda":"licious"}
+    return {"Current queue":str(customerQueue)}
 
-@app.route('/{name}')
-def hello_name(name):
-   return {'hello': name}
+@app.route('/reset', methods=['POST']) 
+def reset():
+    customerQueue.clear()
+    return {"Queue reset":str(customerQueue)}
+
+@app.route('/add/{name}', methods=['POST'])
+def add(name):
+    customerQueue.append(name)
+    return {"Added to queue":str(customerQueue)}
+
+@app.route('/remove', methods=['POST']) 
+def remove():
+    customerQueue.pop(0)
+    return {"Removed from queue":str(customerQueue)}
+
+@app.route('/notify', methods=['POST'])
+def notify():
+    if len(customerQueue) > 0:
+        return {"Next in queue":customerQueue[0]}
+    else:
+        return {"Next in queue":"No one in queue"}
+    
+@app.route('/next', methods=['POST'])
+def next():
+    if len(customerQueue) > 0:
+        customerQueue.pop(0)
+        return {"Next in queue":customerQueue[0]}
+    else:
+        return {"Next in queue":"No one in queue"}
+    
+@app.route("/stats")
+def stats():
+    return {"Queue length":len(customerQueue)}
+
+@app.route('/position/{name}', methods=['GET'])
+def position(name):
+    if name in customerQueue:
+        return {"Position in queue":customerQueue.index(name)}
+    else:
+        return {"Position in queue":"Not in queue"}
