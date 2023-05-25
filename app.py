77e0527d08fe7d8fdf23ce64ae0679be9b01984f
Adam Byrne <adamrbyrne@gmail.com>
2023-05-25 12:27:03 +0100

    refactor

--- app.py
+++ app.py
@@ -11,49 +11,56 @@ API_KEY = '1234'
 def index():
     return {"Current queue":str(customerQueue)}
 
-@app.route('/reset', methods=['POST']) 
+@app.route('/reset', methods=['DELETE'], api_key_required=True) 
 def reset():
-    customerQueue.clear()
-    return {"Queue reset":str(customerQueue)}
-
-@app.route('/add/{name}', methods=['POST'], api_key_required=True)
-def add(name):
-    if ( API_KEY != app.current_request.headers['x-api-key'] ):
+    if (API_KEY != app.current_request.headers['x-api-key'] ):
         return {"Error":"Invalid API key"}
     
-    customerQueue.append(name)
-    return {"Added to queue":str(customerQueue)}
+    customerQueue.clear()
+    return {"status":"success"}, 204
 
-@app.route('/remove', methods=['POST'], api_key_required=True) 
+@app.route('/add/{name}', methods=['PUT'], api_key_required=True)
+def add(name):
+    if ( API_KEY != app.current_request.headers['x-api-key'] ):
+        return {"Error":"Invalid API key"}, 401
+    
+    customerQueue.append(name)
+    return {"Added to queue":str(customerQueue)}, 200
+
+@app.route('/remove', methods=['DELETE'], api_key_required=True) 
 def remove():
     if ( API_KEY != app.current_request.headers['x-api-key'] ):
-        return {"Error":"Invalid API key"} 
+        return {"Error":"Invalid API key"} , 401
     
     customerQueue.pop(0)
-    return {"Removed from queue":str(customerQueue)}
+    return {"status":"success"}, 204
 
-@app.route('/notify', methods=['POST'])
+@app.route('/notify', methods=['POST'], api_key_required=True)
 def notify():
-    if len(customerQueue) > 0:
-        return {"Next in queue":customerQueue[0]}
-    else:
-        return {"Next in queue":"No one in queue"}
+    if ( API_KEY != app.current_request.headers['x-api-key'] ):
+        return {"Error":"Invalid API key"} , 401
     
-@app.route('/next', methods=['POST'])
+    if len(customerQueue) > 0:
+        return {"Next in queue":customerQueue[0]}, 200
+    return {"Next in queue":"No one in queue"}, 200
+    
+@app.route('/next', methods=['DELETE'], api_key_required=True)
 def next():
+    if ( API_KEY != app.current_request.headers['x-api-key'] ):
+        return {"Error":"Invalid API key"} , 401
     if len(customerQueue) > 0:
         customerQueue.pop(0)
-        return {"Next in queue":customerQueue[0]}
+        return {"Next in queue":customerQueue[0]}, 200
     else:
-        return {"Next in queue":"No one in queue"}
+        return {"Next in queue":"No one in queue"}, 200
     
 @app.route("/stats")
 def stats():
-    return {"Queue length":len(customerQueue)}
+    return {"Queue length":len(customerQueue)}, 200
 
 @app.route('/position/{name}', methods=['GET'])
 def position(name):
     if name in customerQueue:
-        return {"Position in queue":customerQueue.index(name)}
+        return {"Position in queue":customerQueue.index(name)}, 200
     else:
-        return {"Position in queue":"Not in queue"}
+        return {"Not in queue"}, 204
