e6d487b9d7d036129d00cab89ebd182a26414f7c
Adam Byrne <adamrbyrne@gmail.com>
2023-05-26 10:08:32 +0100

    fixed codes and auth

--- app.py
+++ app.py
@@ -1,66 +1,62 @@
-from chalice import Chalice
+from chalice import Chalice, Response
 
 app = Chalice(app_name='lambdalicious')
 
 customerQueue = []
 
-API_KEY = '1234'
+API_KEYS = ['1234']
 
+def check_ak(func):
+    def wrapper(*args, **kwargs):
+        api_key = app.current_request.headers.get('x-api-key')
+        if api_key not in API_KEYS:
+            return Response('Invalid API key', status_code=401)
+        return func(*args, **kwargs)
+    return wrapper
 
 @app.route('/', methods=['GET'])
 def index():
-    return {"Current queue":str(customerQueue)}
+    return Response(body=str(customerQueue), status_code=200)
 
 @app.route('/reset', methods=['DELETE'], api_key_required=True) 
+@check_ak
 def reset():
-    if (API_KEY != app.current_request.headers['x-api-key'] ):
-        return {"Error":"Invalid API key"}
-    
     customerQueue.clear()
-    return {"status":"success"}, 204
+    return Response(body="Queue reset", status_code=204)
 
 @app.route('/add/{name}', methods=['PUT'], api_key_required=True)
+@check_ak
 def add(name):
-    if ( API_KEY != app.current_request.headers['x-api-key'] ):
-        return {"Error":"Invalid API key"}, 401
-    
     customerQueue.append(name)
-    return {"Added to queue":str(customerQueue)}, 200
+    return Response(body="Client added", status_code=204)
 
-@app.route('/remove', methods=['DELETE'], api_key_required=True) 
-def remove():
-    if ( API_KEY != app.current_request.headers['x-api-key'] ):
-        return {"Error":"Invalid API key"} , 401
-    
-    customerQueue.pop(0)
-    return {"status":"success"}, 204
+@app.route('/remove/{name}', methods=['DELETE'], api_key_required=True) 
+@check_ak
+def remove(name):
+    customerQueue.remove(name)
+    return Response(body="Client removed", status_code=204)
 
 @app.route('/notify', methods=['POST'], api_key_required=True)
+@check_ak
 def notify():
-    if ( API_KEY != app.current_request.headers['x-api-key'] ):
-        return {"Error":"Invalid API key"} , 401
-    
     if len(customerQueue) > 0:
-        return {"Next in queue":customerQueue[0]}, 200
-    return {"Next in queue":"No one in queue"}, 200
+        return Response(body=customerQueue[0], status_code=200)
+    return Response(body='No one in queue', status_code=204)
     
 @app.route('/next', methods=['DELETE'], api_key_required=True)
+@check_ak
 def next():
-    if ( API_KEY != app.current_request.headers['x-api-key'] ):
-        return {"Error":"Invalid API key"} , 401
     if len(customerQueue) > 0:
         customerQueue.pop(0)
-        return {"Next in queue":customerQueue[0]}, 200
-    else:
-        return {"Next in queue":"No one in queue"}, 200
+        return Response(body=customerQueue[0], status_code=200)
+    return Response(body='No one in queue', status_code=204)
     
 @app.route("/stats")
 def stats():
-    return {"Queue length":len(customerQueue)}, 200
+    return Response(body=str(len(customerQueue)), status_code=200)
 
 @app.route('/position/{name}', methods=['GET'])
 def position(name):
     if name in customerQueue:
-        return {"Position in queue":customerQueue.index(name)}, 200
-    else:
-        return {"Not in queue"}, 204
+        return Response(body=str(customerQueue.index(name)), status_code=200)
+    return Response(body='Client not in queue', status_code=204)
