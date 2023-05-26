9750a4176ef0cc6ab1a74e869b610d40f701aa39
Adam Byrne <adamrbyrne@gmail.com>
2023-05-26 10:34:13 +0100

    dynamo

--- app.py
+++ app.py
@@ -1,8 +1,43 @@
 from chalice import Chalice, Response
+import boto3
 
 app = Chalice(app_name='lambdalicious')
 
-customerQueue = []
+class DynamoDBConfig:
+    TABLE_NAME = 'QueueManagement'
+    READ_CAPACITY_UNITS = 5
+    WRITE_CAPACITY_UNITS = 5
+
+dynamodb = None
+customerQueueTable = None
+
+def initialize_dynamodb(config):
+    global dynamodb, customerQueueTable
+    dynamodb = boto3.resource('dynamodb')
+    customerQueueTable = dynamodb.Table(config.TABLE_NAME)
+
+def get_customer_queue():
+    response = customerQueueTable.get_item(Key={'index': 1})
+    item = response.get('Item')
+    if item:
+        return item.get('queue', [])
+    else:
+        return []
+
+def update_customer_queue(queue):
+    customerQueueTable.put_item(Item={'index': 1, 'queue': queue})
+
+def pop_customer_queue():
+    customer_queue = get_customer_queue()
+    if len(customer_queue) > 0:
+        customer = customer_queue.pop(0)
+        update_customer_queue(customer_queue)
+        return customer
+    else:
+        return None
+
+def initialize_customer_queue():
+    update_customer_queue([])
 
 API_KEYS = ['1234']
 
@@ -16,47 +51,69 @@ def check_ak(func):
 
 @app.route('/', methods=['GET'])
 def index():
-    return Response(body=str(customerQueue), status_code=200)
+    customer_queue = get_customer_queue()
+    return Response(body=str(customer_queue), status_code=200)
 
-@app.route('/reset', methods=['DELETE'], api_key_required=True) 
+@app.route('/reset', methods=['DELETE'], api_key_required=True)
 @check_ak
 def reset():
-    customerQueue.clear()
+    initialize_customer_queue()
     return Response(body="Queue reset", status_code=204)
 
 @app.route('/add/{name}', methods=['PUT'], api_key_required=True)
 @check_ak
 def add(name):
-    customerQueue.append(name)
+    customer_queue = get_customer_queue()
+    customer_queue.append(name)
+    update_customer_queue(customer_queue)
     return Response(body="Client added", status_code=204)
 
-@app.route('/remove/{name}', methods=['DELETE'], api_key_required=True) 
+@app.route('/remove/{name}', methods=['DELETE'], api_key_required=True)
 @check_ak
 def remove(name):
-    customerQueue.remove(name)
-    return Response(body="Client removed", status_code=204)
+    customer_queue = get_customer_queue()
+    if name in customer_queue:
+        customer_queue.remove(name)
+        update_customer_queue(customer_queue)
+        return Response(body="Client removed", status_code=204)
+    else:
+        return Response(body="Client not in queue", status_code=204)
 
 @app.route('/notify', methods=['POST'], api_key_required=True)
 @check_ak
 def notify():
-    if len(customerQueue) > 0:
-        return Response(body=customerQueue[0], status_code=200)
-    return Response(body='No one in queue', status_code=204)
+    customer_queue = get_customer_queue()
+    if len(customer_queue) > 0:
+        customer = customer_queue[0]
+        return Response(body=customer, status_code=200)
+    else:
+        return Response(body="No one in queue", status_code=204)
     
 @app.route('/next', methods=['DELETE'], api_key_required=True)
 @check_ak
 def next():
-    if len(customerQueue) > 0:
-        customerQueue.pop(0)
-        return Response(body=customerQueue[0], status_code=200)
+    if len(get_customer_queue()) > 0:
+        pop_customer_queue()
+        return Response(body=get_customer_queue()[0], status_code=200)
     return Response(body='No one in queue', status_code=204)
-    
+
 @app.route("/stats")
 def stats():
-    return Response(body=str(len(customerQueue)), status_code=200)
+    return Response(body=str(len(get_customer_queue())), status_code=200)
 
 @app.route('/position/{name}', methods=['GET'])
 def position(name):
-    if name in customerQueue:
-        return Response(body=str(customerQueue.index(name)), status_code=200)
+    if name in get_customer_queue():
+        return Response(body=str(get_customer_queue().index(name)), status_code=200)
     return Response(body='Client not in queue', status_code=204)
+
+initialize_dynamodb(DynamoDBConfig())
+
+
+
+    
+
+    
+
+
+
